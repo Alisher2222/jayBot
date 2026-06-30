0b1da482c9d555b059801107a46b9f077efe23f3
Alisher2222 <alisherasanov05@gmail.com>
2026-06-30 19:25:09 +0500

    added myself as an example

--- config/schedule.js
+++ config/schedule.js
@@ -6,6 +6,6 @@ export const TIMEZONE = process.env.TIMEZONE || "Asia/Almaty";
 // Дни: 0=Вс, 1=Пн, 2=Вт, 3=Ср, 4=Чт, 5=Пт, 6=Сб
 export const STUDENT_SCHEDULES = {
   "-4560939511": {
-    2: ["19:50", "20:30"],
+    2: ["19:36", "20:30"],
   },
 };
