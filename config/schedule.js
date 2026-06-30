27d84b870930857b8903aaf10ef1526d771c651d
Alisher2222 <alisherasanov05@gmail.com>
2026-06-30 19:23:00 +0500

    added myself as an example

--- config/schedule.js
+++ config/schedule.js
@@ -4,4 +4,8 @@ export const TIMEZONE = process.env.TIMEZONE || "Asia/Almaty";
 // Расписание каждого ученика
 // Ключ — Chat ID (строка), значение — объект { день: ['HH:MM', ...] }
 // Дни: 0=Вс, 1=Пн, 2=Вт, 3=Ср, 4=Чт, 5=Пт, 6=Сб
-export const STUDENT_SCHEDULES = {};
+export const STUDENT_SCHEDULES = {
+  "-4560939511": {
+    2: ["19:50", "20:30"],
+  },
+};
