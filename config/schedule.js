d9577d7b72ce050b6ea4d9fa08fbebe4b99e266f
Alisher2222 <alisherasanov05@gmail.com>
2026-06-30 19:27:41 +0500

    testing try 1

--- config/schedule.js
+++ config/schedule.js
@@ -1,11 +1,11 @@
 // Часовой пояс
-export const TIMEZONE = process.env.TIMEZONE || "Asia/Almaty";
+export const TIMEZONE = process.env.TIMEZONE || "Asia/Astana";
 
 // Расписание каждого ученика
 // Ключ — Chat ID (строка), значение — объект { день: ['HH:MM', ...] }
 // Дни: 0=Вс, 1=Пн, 2=Вт, 3=Ср, 4=Чт, 5=Пт, 6=Сб
 export const STUDENT_SCHEDULES = {
   "-4560939511": {
-    2: ["19:36", "20:30"],
+    2: ["19:40", "20:30"],
   },
 };
